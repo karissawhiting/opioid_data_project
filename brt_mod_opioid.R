c21481331895674b3b778e39469660067f69b644
Karissa <whiting@ecohealthalliance.org>
2018-04-28 17:40:27 -0400

    model iteration

--- brt_mod_opioid.R
+++ brt_mod_opioid.R
@@ -5,12 +5,20 @@ library(tree)
 o <- read_csv(file = "./data/clean_data.csv")
 
 o <- o %>%
-  dplyr::select(-X1, -State, -Abrev, -MH_Spending) %>%
+  dplyr::select(-X1, -State, -Abrev, -MH_Spending, -Op_Prescribers1, -Homeless, -Alldrug_OD) %>%
   mutate(Med_Exp= as.factor(Med_Exp))
 
 map(o, ~sum(is.na(.x)))
 
 tree.op <- tree(Overdose ~ ., data = o)
 summary(tree.op)
+
 plot(tree.op)
-text
+text(tree.op, pretty = 0)
+
+important(tree.op)
+
+
+lin.mod <- lm(Overdose ~ ., data = o)
+
+summary(lin.mod)
